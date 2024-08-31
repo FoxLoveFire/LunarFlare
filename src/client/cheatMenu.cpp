eedf46764f86aa29fa4deb5210c74b9fce42f9dd
Maintainer_ <131448485+FoxLoveFire@users.noreply.github.com>
2024-08-31 15:25:06 +0500

    Fix error with irrlicht.h

--- src/client/cheatMenu.cpp
+++ src/client/cheatMenu.cpp
@@ -22,7 +22,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "client/fontengine.h"
 #include "cheatMenu.h"
 #include <cstddef>
-#include <irrlicht/irrlicht.h>
+//#include <irrlicht/irrlicht.h>
 
 FontMode Menu::fontStringToEnum(std::string str)
 {
